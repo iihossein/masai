29cdbb1bba22a1ac3f1138fcbfd77969b3bee88f
iihossein <hosseinnazarigit@gmail.com>
2023-08-15 14:17:23 +0330

    user table added & Fortify installed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,22 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username',100)->nullable()->unique();
+            $table->string('first_name',40);
+            $table->string('last_name',60);
+            $table->string('email')->unique()->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('auth_provider_id')->nullable();
+            $table->string('provider_token')->nullable();
+            $table->string('phone',12);
+            $table->string('avatar')->nullable();
+            $table->timestamp('birthday');
+            $table->tinyInteger('gender');
+            $table->tinyInteger('is_active')->default(1);
+            $table->tinyInteger('status');
+            $table->integer('score')->nullable();
+            $table->tinyInteger('rule_id')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
